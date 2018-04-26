4f83b1c7c8ae0a6fd231238d0599bec62c120a86
Dalak <dalak.bartek@gmail.com>
2018-04-26 19:11:15 +0200

    Moving with arrow

--- server.c
+++ server.c
@@ -13,19 +13,20 @@
 #define BUFFER_SIZE   2000
 #define QUEUE_LENGTH     5
 
-char buf1[] = {255, 253, 34};
-char buf2[] = {255, 250, 34, 1, 0, 255, 240};
+char negotiation[] = {IAC, DO, TELOPT_LINEMODE};
+char buf2[] = {IAC, SB, TELOPT_LINEMODE, TELOPT_ECHO, TELQUAL_SEND, 255, 240};
+// char NEG[] = {255, 251, 1};
+char NEG[] = {IAC, SB, TELOPT_DET, 8, IAC, SE};
+char NEG2[] ={IAC, SB, TELOPT_DET, 5, 1, 0, IAC, SE};
+char clear_terminal[] = "\e[1;1H\e[2J";
 char CLEAR_SIGN[]={0x1B,'c','\0'};
 
+char menu_chosen_a[] = "-->Opcja A\n   Opcja B\n   Koniec\n";
+char menu_chosen_b[] = "   Opcja A\n-->Opcja B\n   Koniec\n";
+char menu_chosen_end[] = "   Opcja A\n   Opcja B\n-->Koniec\n";
 
-char NEG[] = "\377\375\042\377\373\001";
-
-char *menu_chosen_a = "-->Opcja A\n   Opcja B\n   Koniec\n";
-char *menu_chosen_b = "   Opcja A\n-->Opcja B\n   Koniec\n";
-char *menu_chosen_end = "   Opcja A\n   Opcja B\n-->Koniec\n";
-
-char UP_ARROW[] = {27, 91, 65, '\0'};
-char DOWN_ARROW[] = {27, 91, 66, '\0'};
+char UP_ARROW[] = {27, 91, 65};
+char DOWN_ARROW[] = {27, 91, 66};
 
 char buffer[BUFFER_SIZE];
 
@@ -39,6 +40,16 @@ bool is_number(char *arr) {
     }
 
     return res == 0 ? false : true;
+}
+
+bool is_up_arrow(ssize_t len) {
+    return len == 3 && buffer[0] == UP_ARROW[0] && buffer[1] == UP_ARROW[1] 
+            && buffer[2] == UP_ARROW[2];
+}
+
+bool is_down_arrow(ssize_t len) {
+    return len == 3 && buffer[0] == DOWN_ARROW[0] && buffer[1] == DOWN_ARROW[1] 
+            && buffer[2] == DOWN_ARROW[2];
 }
 
 int main(int argc, char *argv[]) {
@@ -76,38 +87,43 @@ int main(int argc, char *argv[]) {
     socklen_t client_address_len;
     struct sockaddr_in client_address;
     int msg_sock;
-    int curr_option = 0;
 
     for (;;) {
         client_address_len = sizeof(client_address);
         msg_sock = accept(sock, (struct sockaddr *) &client_address, &client_address_len);
-        int n = send(msg_sock, NEG, strlen(NEG), 0);
-        // n = send(msg_sock, buf2, 7, 0);
 
         if (msg_sock < 0)
             syserr("Accept");
 
-        snd_len = write(msg_sock, menu_chosen_a, strlen(menu_chosen_a));
+        int curr_option = 0;
+        snd_len = write(msg_sock, negotiation, sizeof(negotiation));
+        // snd_len = write(msg_sock, menu_chosen_a, sizeof(menu_chosen_a));
 
         do {
-            len = read(msg_sock, buffer, sizeof(buffer));
-            char xd[3];
-            strncpy(xd, buffer, 3);
-            printf("%d %d\n", strlen(UP_ARROW), strlen(DOWN_ARROW));
-            if (strcmp(xd, UP_ARROW) == 0) {
-                curr_option = curr_option == 0 ? 2 : curr_option - 1;
-            } else if (strcmp(xd, DOWN_ARROW) == 0) {
-                curr_option++;
-                curr_option = curr_option % 3;
+            snd_len = write(msg_sock, clear_terminal, sizeof(clear_terminal));
+
+            if (curr_option == 0) {
+                snd_len = write(msg_sock, menu_chosen_a, sizeof(menu_chosen_a));
+            } else if (curr_option == 1) {
+                snd_len = write(msg_sock, menu_chosen_b, sizeof(menu_chosen_b));
+            } else {
+                snd_len = write(msg_sock, menu_chosen_end, sizeof(menu_chosen_end));
             }
 
-            n = send(msg_sock, CLEAR_SIGN, strlen(CLEAR_SIGN), 0);
-            if (curr_option == 0) {
-                snd_len = write(msg_sock, menu_chosen_a, strlen(menu_chosen_a));
-            } else if (curr_option == 1) {
-                snd_len = write(msg_sock, menu_chosen_b, strlen(menu_chosen_b));
-            } else {
-                snd_len = write(msg_sock, menu_chosen_end, strlen(menu_chosen_end));
+            len = read(msg_sock, buffer, sizeof(buffer));
+            // write(1, "du[a", 4);
+            // if (len < 0)
+                // syserr("Reading from client socker");
+
+            // printf("%d %d %d %d\n", len, sizeof(UP_ARROW), strlen(UP_ARROW), sizeof(DOWN_ARROW));
+            // // char xd[3];
+            // // strncpy(xd, buffer, 3);
+
+            if (is_up_arrow(len)) {
+                curr_option = curr_option == 0 ? 2 : curr_option - 1;
+            } else if (is_down_arrow(len)) {
+                curr_option++;
+                curr_option = curr_option % 3;
             }
         } while (len > 0);
         
